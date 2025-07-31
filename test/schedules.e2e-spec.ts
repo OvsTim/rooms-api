f8e75eabe815a9871dfdc299d0fe39745621c7ab
ovstim <ovstim@mail.ru>
2025-07-31 11:42:58 +0300

    fix: deleted beforeEach

--- test/schedules.e2e-spec.ts
+++ test/schedules.e2e-spec.ts
@@ -188,14 +188,6 @@ describe('AppController (e2e)', () => {
       });
   });
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
 
   afterAll(async () => {
     await request(app.getHttpServer())
